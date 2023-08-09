5dddc118802fe8fc80e246b00912023376bbc82a
Tim Twigg <tim@twiggusa.com>
2023-08-08 17:03:20 -0700

    Add Middle Earth map

--- src/pages/maps.tsx
+++ src/pages/maps.tsx
@@ -10,21 +10,35 @@ const MapsPage: React.FC<PageProps> = () => {
     return (
         <Layout title = "Maps">
             <h1>Maps</h1>
-            <p>
-                Maps created with Inkarnate.
-            </p>
             <AnchorLinkBar hideWhenSmall = {true} pageTitle = "Maps" links = {[
+                { name: "Middle Earth", url: "/maps#middleEarth" },
                 { name: "Vales of Anduin", url: "/maps#valesOfAnduin" }
             ]}/>
             <hr/>
 
+            <h3 id = "middleEarth">Middle Earth</h3>
+            <p>
+                Map created on DeviantArt by <a href = "https://www.deviantart.com/caeora" target = "_blank">Caeora</a>.
+            </p>
+            <div className = "ten columns offset-by-one column">
+                <Zoom>
+                    <StaticImage imgStyle={{height: "100%", width: "100%"}} src = "../assets/middleEarthMap.jpg" alt = "Map of Middle Earth"/>
+                </Zoom>
+            </div>
+            <div className = "spacer"/>
+            <hr/>
+
             <h3 id = "valesOfAnduin">Vales of Anduin</h3>
+            <p>
+                Map created with Inkarnate by me.
+            </p>
             <div className = "ten columns offset-by-one column">
                 <Zoom>
                     <StaticImage imgStyle = {{height: "100%", width: "100%"}} src = "../assets/valesOfAnduinMap.jpg" alt = "Map of the Vales of Anduin"/>
                 </Zoom>
             </div>
             
+            <div className = "double spacer"/>
             <div className = "double spacer"/>
         </Layout>
     )
